a1a01f7fc60105416ca0c05080faa61e8d682a85
enw12345 <1evan.williams1@gmail.com>
2020-09-28 12:46:40 -0400

    graphs done. on to mathematical surfaces

--- Assets/Graph/Graph.cs
+++ Assets/Graph/Graph.cs
@@ -1,17 +1,51 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
-    public Transform pointPrefab;
+    [SerializeField]
+    private Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    private Transform[] points;
+
+    public int Resolution
+    {
+        get => resolution;
+        set => Mathf.Clamp(resolution, 10, 100);
+    }
 
     void Awake()
     {
-        int i = 0;
-        while (i < 10)
+        float step = 2f / resolution;
+        Vector3 position = Vector3.zero;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution];
+
+        for (int i = 0; i < points.Length; i++)
         {
-            i = i + 1;
             Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right * i;
+            point.SetParent(transform, false);
+
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            points[i] = point;
+            Debug.Log(point.localPosition);
+        }
+    }
+
+    private void Update()
+    {
+        float time = Time.time;
+        for(int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
         }
     }
 }
