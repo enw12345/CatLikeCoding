92c2f3cbe091e85c35e67147381dd26a2effe057
enw12345 <1evan.williams1@gmail.com>
2020-09-29 03:33:31 -0400

    mathematical surfaces updated

--- Assets/Graph/Graph.cs
+++ Assets/Graph/Graph.cs
@@ -9,6 +9,9 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+
     private Transform[] points;
 
     public int Resolution
@@ -21,31 +24,36 @@ public class Graph : MonoBehaviour
     {
         float step = 2f / resolution;
         Vector3 position = Vector3.zero;
-        var scale = Vector3.one * step;
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
+        Vector3 scale = Vector3.one * step;
 
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = Instantiate(pointPrefab);
-            point.SetParent(transform, false);
-
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform, false);
             points[i] = point;
-            Debug.Log(point.localPosition);
         }
     }
 
     private void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        for(int i = 0; i < points.Length; i++)
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-            point.localPosition = position;
+            if(x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.25f) * step - 1f;
+            }
+
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
